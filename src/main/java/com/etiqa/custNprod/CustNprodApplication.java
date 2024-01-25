d23bbae14610669d582c5ec6cdb0ad461fb9cce7
Muhammad Fauzan Syahmi Bin Sha'ari <fauzan.shaari@gameconomy.tech>
2024-01-25 17:15:16 +0800

    Intial Project commit

---
+++ src/main/java/com/etiqa/custNprod/CustNprodApplication.java
@@ -0,0 +1,13 @@
+package com.etiqa.custNprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustNprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustNprodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etiqa/custNprod/CustNprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etiqa.custNprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustNprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
