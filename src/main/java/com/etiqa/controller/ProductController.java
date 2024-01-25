00c4ac9c5905eb9e1960722dd901c485eb3b259f
Muhammad Fauzan Syahmi Bin Sha'ari <fauzan.shaari@gameconomy.tech>
2024-01-25 17:57:27 +0800

    Product Contoller Service Import

--- src/main/java/com/etiqa/controller/ProductController.java
+++ src/main/java/com/etiqa/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.etiqa.service.CustomerService;
+import com.etiqa.service.ProductService;
 
 @RestController
 @RequestMapping("")
@@ -15,6 +15,6 @@ public class ProductController {
     private final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
-    private CustomerService customerService;
+    private ProductService productService;
     
 }
