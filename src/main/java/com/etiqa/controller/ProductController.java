9d2b14f48d6cafcba574df5025c58a960cbb9847
Fauzan Sha'ari <fauzan.syahmi.shaari@gmail.com>
2024-01-28 21:57:39 +0800

    Product Controller & Service

--- src/main/java/com/etiqa/controller/ProductController.java
+++ src/main/java/com/etiqa/controller/ProductController.java
@@ -1,20 +1,82 @@
 package com.etiqa.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.etiqa.domain.Product;
 import com.etiqa.service.ProductService;
+import com.etiqa.service.dto.ProductDTO;
+
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/prodAPI")
 public class ProductController {
 
     private final Logger log = LoggerFactory.getLogger(ProductController.class);
 
     @Autowired
     private ProductService productService;
+
+    @PostMapping("bookRegistration")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void bookData(@Valid @RequestBody ProductDTO productDTO) {
+        log.info("REST request to create book {}", productDTO);
+
+        productService.bookData(productDTO);
+    }
+
+    @GetMapping("/books")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> getAllBookData() {
+        log.info("REST request to find all books {}");
+
+        List<Product> books = productService.getAllBookData() ;
+
+        return books;
+    }
+
+    @GetMapping("/book/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Product> getBookData(@Valid @PathVariable Long id) {
+        log.info("REST request to find book By Id {}", id);
+
+        Product books = productService.getBookData(id) ;
+
+        return ResponseEntity.ok(books);
+    }
+
+    @PutMapping("/book/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Product> updateBookData(@Valid @PathVariable ProductDTO productDTO) {
+        log.info("REST request to update book By Id {}", productDTO.getId());
+
+        Product books = productService.updateBookData(productDTO) ;
+
+        return ResponseEntity.ok(books);
+    }
+
+    @DeleteMapping("/book/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteBookData(@Valid @PathVariable Long id) {
+        log.info("REST request to delete book By Id {}", id);
+
+        productService.deleteBookData(id) ;
+    }
+    
     
 }
--- src/main/java/com/etiqa/service/ProductService.java
+++ src/main/java/com/etiqa/service/ProductService.java
@@ -1,12 +1,86 @@
 package com.etiqa.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.etiqa.domain.Product;
+import com.etiqa.helper.ExceptionHandler;
+import com.etiqa.repository.ProductRepository;
+import com.etiqa.service.dto.ProductDTO;
 
 @Service
 public class ProductService {
 
     private final Logger log = LoggerFactory.getLogger(ProductService.class);
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void bookData(ProductDTO productDTO) {
+
+        Optional<Product> existBookTitle = productRepository.findOneByTitle(productDTO.getTitle());
+
+        if (existBookTitle.isPresent()) {
+            Optional<Product> existBookAuthor = productRepository.findOneByAuthor(productDTO.getTitle());
+
+            if (existBookAuthor.isPresent()) {
+                throw new ExceptionHandler("Book with the same author already exist");
+            }
+        }
+
+        Product book = new Product();
+        book.setTitle(productDTO.getTitle());
+        book.setPrice(productDTO.getPrice());
+        book.setQuantity(productDTO.getQuantity());
+    }
+
+    public Product updateBookData(ProductDTO productDTO) {
+        Product book = productRepository.findById(productDTO.getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No product found with id " + productDTO.getId()));
+
+        log.info("Current book data {}", book);
+
+        book.setTitle(productDTO.getTitle());
+        // book.setAuthor(productDTO);
+        book.setPrice(productDTO.getPrice());
+        book.setQuantity(productDTO.getQuantity());
+
+        return book;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Product> getAllBookData(){
+        List<Product> books = new ArrayList<Product>();
+        
+        books = productRepository.findAll();
+
+        if(books.isEmpty()){
+            throw new NullPointerException("There's no book data.");
+        }
+
+        return books;
+    }
+
+    @Transactional(readOnly = true)
+    public Product getBookData(Long id){
+        Product book = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No book found with id " + id));
+
+        return book;
+    }
+
+    public void deleteBookData(Long id){
+        productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No book found with id " + id));
+    }
     
 }
