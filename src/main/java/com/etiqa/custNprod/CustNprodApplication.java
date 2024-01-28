b4d19d7bd1cc72d2605fc1e02dd91120fbf82308
Fauzan Sha'ari <fauzan.syahmi.shaari@gmail.com>
2024-01-29 03:39:29 +0800

    Remove unnecessary scan

--- src/main/java/com/etiqa/custNprod/CustNprodApplication.java
+++ src/main/java/com/etiqa/custNprod/CustNprodApplication.java
@@ -2,15 +2,8 @@ package com.etiqa.custNprod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.etiqa.custNprod.controller.CustomerController;
-import com.etiqa.custNprod.controller.ProductController;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = CustomerController.class)
-@ComponentScan(basePackageClasses = ProductController.class)
-@ComponentScan("com.etiqa.custNprod")
 public class CustNprodApplication {
 
 	public static void main(String[] args) {
