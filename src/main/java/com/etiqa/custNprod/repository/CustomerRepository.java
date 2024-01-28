3d5be4240952f6f44c935b531c54e7e4d5fa40c2
Fauzan Sha'ari <fauzan.syahmi.shaari@gmail.com>
2024-01-29 02:21:02 +0800

    Enable JpaRepo in Repository

--- src/main/java/com/etiqa/custNprod/repository/CustomerRepository.java
+++ src/main/java/com/etiqa/custNprod/repository/CustomerRepository.java
@@ -1,11 +1,14 @@
 package com.etiqa.custNprod.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
 import com.etiqa.custNprod.domain.Customer;
 import java.util.Optional;
 
-
+@Repository
+@EnableJpaRepositories
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByFNameAndLName(String fName, String lName);
--- src/main/java/com/etiqa/custNprod/repository/ProductRepository.java
+++ src/main/java/com/etiqa/custNprod/repository/ProductRepository.java
@@ -3,11 +3,13 @@ package com.etiqa.custNprod.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import com.etiqa.custNprod.domain.Product;
 
 @Repository
+@EnableJpaRepositories
 public interface ProductRepository extends JpaRepository<Product, Long> {
     
     Optional<Product> findOneByTitle(String title);
