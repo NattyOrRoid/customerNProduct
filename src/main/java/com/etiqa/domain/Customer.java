b76c3cc2d74154c45a82006b35b71b88d4c22096
Fauzan Sha'ari <fauzan.syahmi.shaari@gmail.com>
2024-01-28 21:11:05 +0800

    Set Domain for Customer & Product

--- src/main/java/com/etiqa/domain/Customer.java
+++ src/main/java/com/etiqa/domain/Customer.java
@@ -1,20 +1,28 @@
 package com.etiqa.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "")
+@Table(name = "Customer")
 @Getter
 @Setter
 public class Customer {
-
+    @Id
     private long id;
+    @Column(name = "First Name")
     private String fName;
+    @Column(name = "Last Name")
     private String lName;
+    @Column(name = "Office Mail")
     private String officeMail;
+    @Column(name = "Personal Mail")
     private String personalMail;
+    @Column(name = "Family Member")
+    private String famMember;
     
 }
--- src/main/java/com/etiqa/domain/Product.java
+++ src/main/java/com/etiqa/domain/Product.java
@@ -1,19 +1,26 @@
 package com.etiqa.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "")
+@Table(name = "Product")
 @Getter
 @Setter
 public class Product {
-
+    @Id
     private long id;
+    @Column(name = "Book Title")
     private String title;
+    @Column(name = "Book Auhtor")
+    private String author;
+    @Column(name = "Book Price")
     private float price;
+    @Column(name = "Book Quantity")
     private int quantity;
     
 }
